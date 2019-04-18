2d09166b862c7287f29bc6ff5d3021affdbee0b9
Jin <celine.jkim@gmail.com>
2019-04-18 19:53:13 -0400

    changes

---
+++ app_1/static/js/adoptapet.js
@@ -0,0 +1,29 @@
+
+//Let's remember what you select
+var species = "";
+if ($("h1").text() == "🐶"){
+    species = "Dog";
+} else if ($("h1").text() == "🐱"){
+    species = "Cat";
+} else {
+
+}
+
+var breed = "";
+var size = "";
+var gender = "";
+var age = "";
+var color = "";
+
+var search_key = {
+    "species": species,
+    "breed": breed,
+    "size": size,
+    "gender": gender,
+    "age": age,
+    "color": color
+};
+
+
+
+
