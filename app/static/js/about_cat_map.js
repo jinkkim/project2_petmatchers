d936da5cc038719093b9a784f77d2fd0a5db93f5
Andrew Boring <andrew@andrewboring.com>
2019-04-18 19:18:57 -0400

    merging Jins changes

---
+++ app/static/js/about_cat_map.js
@@ -0,0 +1,76 @@
+function drawMap(cat_population){
+
+    //create map
+    //reference  : https://leafletjs.com/examples/choropleth/
+
+
+    function getColor(d) {
+      return d > 800 ? '#800026' :
+             d > 600  ? '#BD0026' :
+             d > 400  ? '#E31A1C' :
+             d > 200  ? '#FC4E2A' :
+             d > 100   ? '#FD8D3C' :
+             d > 50   ? '#FEB24C' :
+             d > 10   ? '#FED976' :
+                        '#FFEDA0';
+    }
+
+    function style(feature) {
+      return {
+          fillColor: getColor(feature.properties.density),
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+      };
+  }
+
+    var myMap = L.map("map_cat", {
+      center: [40.13, -94.93],
+      zoom: 5
+    });
+
+    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+    }).addTo(myMap);
+
+    //correct populaton in goejson to dog population
+    statesData.features.forEach(function(feature){
+      feature.properties.density = cat_population[feature.properties.name]
+    })
+
+    var geojson = L.geoJson(statesData, {style: style}).addTo(myMap);
+
+
+
+
+
+    // insert legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend'),
+          grades = [0, 10, 50, 100, 200, 400, 600, 800],
+          labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+
+      return div;
+    };
+
+    legend.addTo(myMap);
+    
+
+    
+
+
+}
