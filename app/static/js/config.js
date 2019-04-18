07b358b5a13c2a1dc644743112885aae2572c8f7
Andrew Boring <andrew@andrewboring.com>
2019-04-18 19:33:00 -0400

    move app_1 to app

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoidGhvbGxhIiwiYSI6ImNqdTVrMnZ4dTBwano0NHFreW11MnFzcGsifQ.UkPXfzyD7ASDuz_CytljNw";
+const API_KEY = "pk.eyJ1IjoicGZlc3VuIiwiYSI6ImNqdWNua3ZrZzA4Nm8zeXRjdDFlamVnZ28ifQ.QqGWmcy-HOa2leXdZ-1ulA";
